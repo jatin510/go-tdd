0617264b5b0b27911f0e4515840788356f152831
jagdishparihar <jagdish.parihar@biofourmis.com>
2022-11-10 19:13:06 +0530

    wallet package

---
+++ wallet/walltet_test.go
@@ -0,0 +1,20 @@
+package wallet
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+
+	wallet.Deposit(Bitcoin(10))
+
+	got := wallet.Balance()
+
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
