48feefcdbd91021b303792a29696022b9991eea9
jagdishparihar <jagdish.parihar@biofourmis.com>
2022-11-10 16:46:04 +0530

    refactor : added area function to rectangle and circle

--- integer/shape_test.go
+++ integer/shape_test.go
@@ -14,12 +14,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10, 10}
 
-	got := Area(rectangle)
-	want := 100.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
 }
