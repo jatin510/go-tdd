de558a68d74913f2fa9f68839b4756b5a05dc9af
jagdishparihar <jagdish.parihar@biofourmis.com>
2022-11-10 23:44:41 +0530

    refactored code for update the dictionary

--- maps/map.go
+++ maps/map.go
@@ -9,8 +9,9 @@ func (e dictionaryErr) Error() string {
 }
 
 var (
-	ErrNotFound   = dictionaryErr("could not find the word you were looking for")
-	ErrWordExists = dictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = dictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = dictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = dictionaryErr("cannot update word because it does not exists")
 )
 
 func (dictionary Dictionary) Search(word string) (string, error) {
@@ -30,6 +31,21 @@ func (d Dictionary) Add(word, definition string) error {
 		d[word] = definition
 	case nil:
 		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
 	default:
 		return err
 	}
--- maps/map_test.go
+++ maps/map_test.go
@@ -45,6 +45,29 @@ func TestAdd(t *testing.T) {
 	})
 }
 
+func TestUpdate(t *testing.T) {
+	t.Run("testing update dictionary", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		newDefinition := "new definition"
+		dictionary.Update(word, newDefinition)
+
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
+
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
