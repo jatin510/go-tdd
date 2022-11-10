41b20083632b6e356278f5af538ffec1e6c451f1
jagdishparihar <jagdish.parihar@biofourmis.com>
2022-11-11 01:07:51 +0530

    added spyCountdownOperations interface

--- mocking/mock.go
+++ mocking/mock.go
@@ -12,12 +12,14 @@ const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 
-	for i := countdownStart; i > 0; i-- {
-		sleeper.Sleep()
-	}
+	// for i := countdownStart; i > 0; i-- {
+	// 	sleeper.Sleep()
+	// }
 
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+
 	}
 
 	fmt.Fprint(out, finalWord)
@@ -45,3 +47,21 @@ type DefaultSleeper struct{}
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
+
+// spying to check the order of implementation
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
--- mocking/mock_test.go
+++ mocking/mock_test.go
@@ -2,6 +2,7 @@ package mocks
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -20,4 +21,37 @@ Go!`
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
