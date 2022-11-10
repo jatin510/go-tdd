348bfb2b9f78539dd7736804772354c0a3e5b5f0
jagdishparihar <jagdish.parihar@biofourmis.com>
2022-11-10 19:25:49 +0530

    added ability to withdraw the btc

--- wallet/walltet.go
+++ wallet/walltet.go
@@ -1,9 +1,15 @@
 package wallet
 
+import "fmt"
+
 type Bitcoin int
 
 type Wallet struct {
 	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
@@ -11,6 +17,9 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 }
 
 func (w *Wallet) Balance() Bitcoin {
-
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
