eeb90889cc57b2b81d4d65a057fc065faee55a87
jagdishparihar <jagdish.parihar@biofourmis.com>
2022-11-10 19:54:53 +0530

    added ability to handle error in dictionary

--- maps/map.go
+++ maps/map.go
@@ -1,5 +1,15 @@
 package maps
 
-func Search(dictionary map[string]string, key string) string {
-	return dictionary[key]
+import "errors"
+
+type Dictionary map[string]string
+
+func (dictionary Dictionary) Search(word string) (string, error) {
+	definition, ok := dictionary[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
+
 }
