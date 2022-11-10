7481e93dc0cb8afdab93ea5cefd9a05565e68016
jagdishparihar <jagdish.parihar@biofourmis.com>
2022-11-10 16:32:01 +0530

    refactored SumAll function

--- integer/sum.go
+++ integer/sum.go
@@ -8,11 +8,10 @@ func Sum(numbers []int) (sum int) {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
+	var sums []int
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 
 	return sums
