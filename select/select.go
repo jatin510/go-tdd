4b58033f83686eb58436b06135b0a40beeff7e50
jagdishparihar <jagdish.parihar@biofourmis.com>
2022-11-11 10:12:06 +0530

    refactor: created new function to measure the response time

--- select/select.go
+++ select/select.go
@@ -6,18 +6,19 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 
 	return b
+}
+
+func measureResponseTime(url string) (responseDuration time.Duration) {
+	start := time.Now()
+	http.Get(url)
+	responseDuration = time.Since(start)
+	return
 }
